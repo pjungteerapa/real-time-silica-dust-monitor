942df9037d8bb198dc2ec4e4e216875a1467b60c
Boss30883 <pjungteerapa@student.unimelb.edu.au>
2026-05-10 22:48:22 +1000

    add login sys

---
+++ public/js/login.js
@@ -0,0 +1,85 @@
+// Your web app's Firebase configuration
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCH2ShglhWUCLoxEMhMV8rdw_GHc7_z17c",
+  authDomain: "real-time-silica-dust-monitor.firebaseapp.com",
+  databaseURL: "https://real-time-silica-dust-monitor-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "real-time-silica-dust-monitor",
+  storageBucket: "real-time-silica-dust-monitor.firebasestorage.app",
+  messagingSenderId: "972708961896",
+  appId: "1:972708961896:web:6aef6fe9334a5a18d8a5dd",
+  measurementId: "G-WVEEPJ1FTC"
+};
+
+// Initialize Firebase
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    updateProfile,
+    signOut,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+
+///////////////////////////////////////////////////////////////////////////////////
+//////////////Check Login
+var user_info = {}
+onAuthStateChanged(auth, (user) => {
+    if (user){
+        const dname = user.nameame;
+        const uid = user.uid;
+        console.log(dname,uid)
+        user_info["name"] = dname;
+        user_info["uid"] = uid;
+        document.getElementById("logout").innerHTML = "Logout as " + dname;
+    }
+    if(user_info.name){
+        document.getElementById("login").parent().hide();
+        // document.getElementById("login").style.display = "none";
+    }else{
+        document.getElementById("logout").parent().hide();
+        // document.getElementById("logout").style.display = "none";
+    }
+})
+
+//////////////////////////////////////////////////////////////////////////////////
+/////////////Login form
+const loginForm = document.getElementById('loginform')
+loginForm.addEventListener('submit', (e) => {
+    // e.preventDefault()
+    const email = loginForm.username.value
+    const password = loginForm.password.value
+    console.log(email,password)
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log(email,password)
+            console.log("success")
+            window.location.href = "/"
+        })
+        .catch((err) => {
+            console.log(err.message)
+            alert(err.code)
+        })
+})
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////Log out
+document.getElementById('logout').addEventListener('click', (e) => {
+    e.preventDefault();
+    signOut(auth)
+        .then(() => {
+            window.location.href = "/";
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
